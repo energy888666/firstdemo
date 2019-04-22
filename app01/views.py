dd4b3a66853238704c912a20ab683ec8b74861e4
zy <zhangyang8873@126.com>
2019-04-22 22:09:28 +0800

    dd

--- app01/views.py
+++ app01/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render,HttpResponse
 
 
 def happy(request):
+<<<<<<< Updated upstream
     return HttpResponse("第一次修改dea")
+=======
+    return HttpResponse("第一次修改")
+def happy2(request):
+    return HttpResponse("第2次修改")
+>>>>>>> Stashed changes
