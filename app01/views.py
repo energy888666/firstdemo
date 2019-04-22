b828a02058748c723850e1aca4d760e495294dda
zy <zhangyang8873@126.com>
2019-04-22 22:17:01 +0800

    更新操作

--- app01/views.py
+++ app01/views.py
@@ -4,10 +4,6 @@ from django.shortcuts import render,HttpResponse
 
 
 def happy(request):
-<<<<<<< Updated upstream
     return HttpResponse("第一次修改d")
-=======
-    return HttpResponse("第一次修改")
 def happy2(request):
     return HttpResponse("第er次修改")
->>>>>>> Stashed changes
