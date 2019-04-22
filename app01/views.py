caaea4885536c8d1e280fa53bea9cb365bc13748
zy <zhangyang8873@126.com>
2019-04-22 22:10:33 +0800

    回到stash状态

--- app01/views.py
+++ app01/views.py
@@ -4,10 +4,4 @@ from django.shortcuts import render,HttpResponse
 
 
 def happy(request):
-<<<<<<< Updated upstream
-    return HttpResponse("第一次修改dea")
-=======
     return HttpResponse("第一次修改")
-def happy2(request):
-    return HttpResponse("第2次修改")
->>>>>>> Stashed changes
